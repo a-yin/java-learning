2e4a666a690c5eb9a9941488ccd01ddbef66038d
lingb <linguobiao0728@foxmail.com>
2018-08-30 11:50:01 +0800

    数据结构与算法之树、二叉树02

--- dsaa/src/main/java/com/lingb/mystudy/dsaa/day06/Node.java
+++ dsaa/src/main/java/com/lingb/mystudy/dsaa/day06/Node.java
@@ -5,43 +5,43 @@ package com.lingb.mystudy.dsaa.day06;
  */
 public class Node {
 
-	/**
-	 * 数据项
-	 */
-	public int data;
+    /**
+     * 数据项
+     */
+    public int data;
 
-	/**
-	 * 数据项
-	 */
-	public String sData;
+    /**
+     * 数据项
+     */
+    public String sData;
 
-	/**
-	 * 左子结点
-	 */
-	public Node leftChild;
+    /**
+     * 左子结点
+     */
+    public Node leftChild;
 
-	/**
-	 * 右子结点
-	 */
-	public Node rightChild;
-	
-	/**
-	 * 带参数的构造方法
-	 *
-	 * @param data
-	 */
-	public Node(int data) {
-		this.data = data;
-	}
+    /**
+     * 右子结点
+     */
+    public Node rightChild;
 
-	/**
-	 * 带参数的构造方法
-	 *
-	 * @param data
-	 */
-	public Node(int data, String sData) {
-		this.data = data;
-		this.sData = sData;
-	}
+    /**
+     * 带参数的构造方法
+     *
+     * @param data
+     */
+    public Node(int data) {
+        this.data = data;
+    }
+
+    /**
+     * 带参数的构造方法
+     *
+     * @param data
+     */
+    public Node(int data, String sData) {
+        this.data = data;
+        this.sData = sData;
+    }
 
 }
--- dsaa/src/main/java/com/lingb/mystudy/dsaa/day06/TreeTest.java
+++ dsaa/src/main/java/com/lingb/mystudy/dsaa/day06/TreeTest.java
@@ -5,20 +5,49 @@ import org.junit.Test;
 public class TreeTest {
 
     @Test
+    public void test1() {
+        BinaryTree tree = new BinaryTree();
+        tree.insert(10, "AA");
+        tree.insert(20, "BB");
+        tree.insert(15, "CC");
+        tree.insert(3, "DD");
+        tree.insert(5, "EE");
+        tree.insert(54, "FF");
+        tree.insert(32, "GG");
+
+        // 前序遍历
+        System.out.println("前序遍历------");
+        tree.frontOrder(tree.root);
+
+        // 中序遍历
+        System.out.println("中序遍历------");
+        tree.inOrder(tree.root);
+
+        // 删除结点
+        tree.delete(20);
+
+        // 后序遍历
+        System.out.println("后台序遍历------");
+        tree.afterOrder(tree.root);
+
+    }
+
+
+    @Test
     public void test() {
         BinaryTree tree = new BinaryTree();
         tree.insert(10, "AA");
         tree.insert(20, "BB");
         tree.insert(15, "CC");
-        tree.insert(13, "DD");
+        tree.insert(3, "DD");
 
         System.out.println(tree.root.data);
         System.out.println(tree.root.rightChild.data);
         System.out.println(tree.root.rightChild.leftChild.data);
         System.out.println(tree.root.leftChild.data);
 
-        Node node = tree.find(3);
-        System.out.println(node.data);
+        Node node = tree.find(20);
+        System.out.println(node.data + "," + node.sData);
     }
 
 }
